dcccc3018a4baa0f1be3c8fa42d008d4fc36638e
Tainá Belvis Garcez <127426024+Tainabelvis@users.noreply.github.com>
2023-10-02 22:45:46 -0300

    Create Entidade Curso.sql

---
+++ Banco de dados/Entidade Curso.sql
@@ -0,0 +1,8 @@
+"nome_curso"	VARCHAR(50),
+	"modelo"	VARCHAR(50),
+	"periodo"	VARCHAR(50),
+	"turma"	VARCHAR(50),
+	"nome_sigla"	VARCHAR(50),
+	"idUNIVERSIDADE"	VARCHAR(50),
+	PRIMARY KEY("nome_curso"),
+	FOREIGN KEY("idUNIVERSIDADE") REFERENCES "UNIVERSIDADE"("CNPJ_universidade")
